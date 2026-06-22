71787da9545380482aaeaef054fd65f13e8e0c61
johnhofmann93 <johnhofmann93@gmail.com>
2026-06-21 22:23:48 -0700

    Changed DARK to physical and GHOST to special.

--- data/types/names.asm
+++ data/types/names.asm
@@ -10,7 +10,7 @@ TypeNames:
 	dw Rock
 	dw Bird
 	dw Bug
-	dw Ghost
+	dw Dark
 	dw Steel
 	assert_table_length UNUSED_TYPES
 
@@ -27,7 +27,7 @@ endr
 	dw Psychic
 	dw Ice
 	dw Dragon
-	dw Dark
+	dw Ghost
 	assert_table_length TYPES_END
 
 Normal:    db "NORMAL@"
